ab0f6059fc9052c5f304af05e63a1d171eb7ac25
haru2401game <haru2401game@gmail.com>
2026-06-23 12:12:23 +0900

    Update update-note-stats.js

--- scripts/update-note-stats.js
+++ scripts/update-note-stats.js
@@ -8,27 +8,8 @@ async function main() {
 
     const html = await response.text();
 
-    console.log("downloaded");
+    console.log(html.substring(0, 5000));
 
-    // 記事数抽出
-    const match = html.match(/記事\s*([0-9]+)件/);
-
-    if (!match) {
-        throw new Error("記事数が取得できません");
-    }
-
-    const articleCount = Number(match[1]);
-
-    console.log(articleCount);
-
-    const stats = {
-        blogPosts: articleCount
-    };
-
-    fs.writeFileSync(
-        "./data/stats.json",
-        JSON.stringify(stats, null, 2)
-    );
 }
 
 main();
